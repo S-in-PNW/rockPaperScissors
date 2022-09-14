1328c4116ccd544a16efdbdac875a259ed9ea634
S-in-PNW <102837974+S-in-PNW@users.noreply.github.com>
2022-09-14 15:46:31 -0700

    add author's note and table of contents

--- javascript.js
+++ javascript.js
@@ -1,3 +1,46 @@
+/* 
+* NOTE ************************************************************************************************************************
+*
+*     This code is almost entirely comtained within one big bad for loop (to act as a counter). In retrospect, some 
+*   aspects of this code are really just me thinking too hard. I don't know if the array at the beginnin was really
+*   a necessary addition, but it works pretty well and I like it there. 
+*   
+*     The code declares the computer's choice using the Math.random function to select a string from an array. It then 
+*   uses the playRound function to compare the computer's string to an input string submitted by the player. Depending
+*   on the strings, the function will release an outcome of 1, -1, or "t".
+*   
+*     A quick if-statement will add one point to the player if the playRound outcome is 1, a point to the computer for -1
+*   and a point towards a tie if "t".
+*
+*     Finally, another if-statement will evaluate the scores and ultimately determine a winner. Logical operators were
+*   used to ensure that the player with the majority points would win the game, regardless of reaching a "best three-out-
+*   of-five" scenario. Upon a win or loss, the for-loop will be broken and the game ends.
+*
+********************************************************************************************************************************
+*
+* TABLE OF CONTENTS 
+*
+*   SCORING VARIABLES
+*
+*   FOR-LOOP COUNTER
+*
+*     - ARRAY
+*
+*     - COMPUTER CHOICE
+*
+*     - PLAYER CHOICE
+*
+*     - PLAYROUND FUNCTION
+*
+*     - IF-STATEMENT SCORE COUNTER
+*
+*     - IF-SATEMENT WIN/LOSS DECLARATION
+*
+*
+*/
+
+
+//scoring variables
 let playerScore = 0;
 let computerScore = 0;
 let tie = 0;
@@ -6,76 +49,73 @@ let tie = 0;
 // counter for the five rounds
 for(var i = 0; i < 5; ++i) {
 
+  //array of choices the computer can choose from
   let choices = Array("rock", "paper", "scissors");
 
   //chooses for the computer at the beginning of the bout
   let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
-  console.log(getComputerChoice); //debugger
+  
+  //console.log(getComputerChoice); //debugger
 
   //allows for user input of choice
   let playerSelection = prompt("What is your choice?");
   playerSelection = playerSelection.toLowerCase();
-  //console.log(playerSelection)
+
+  //console.log(playerSelection) //debugger
 
   //logic tree for player selection versus computer selection
   function playRound (player, computer) {
-    if (player === "rock") {
+    if (player === "rock") { //if player chooses rock
         if (computer === "rock") {
-            //return "It's a tie! Go again!";
-            return "t";
+            return "t"; // tie
         } else if (computer === "paper") {
-            //return "Paper beats Rock, you lose :(";
-            return -1;
+            return -1; // loss
         } else {
-            //return "Rock beats Scissors, you win!!";
-            return 1;
+            return 1; // win
         }
-    } else if (player === "paper") {
+    } else if (player === "paper") { // if player chooses paper
         if (computer === "rock") {
-            //return "Paper beats Rock, you win!!";
-            return 1;
+            return 1; // win
         } else if (computer === "paper") {
-            //return "It's a tie! Go Again!";
-            return "t";
+            return "t"; // tie
         } else {
-            //return "Scissors beats Paper, you lose :(";
-            return -1;
+            return -1; // loss
         }
-    } else if (player === "scissors") {
+    } else if (player === "scissors") { //if player chooses scissors
         if (computer === "rock") {
-           // return "Rock beats Scissors, you lose :(";
-           return -1;
+           return -1; // loss
         } else if (computer === "paper") {
-            //return "Scissors beats Paper, you win!!";
-            return 1;
+            return 1; // win
         } else {
-           // return "It's a tie!";
-           return "t";
+           return "t"; // tie
         }
     } else {
-        return "Hey! No cheating!";
+        return "Hey! No cheating!"; // fallthrough
     }
   }
 
+  // puts result of the funstion in a variable
   let result = playRound(playerSelection, getComputerChoice);
 
+  // keeps score based off of the "result" variable
   if (result === 1) {
-    ++playerScore
+    ++playerScore // point for the player
     console.log("Yay! You got a point!")
   } else if (result === -1) {
-    ++computerScore
+    ++computerScore // point for the computer
     console.log("Darn, lost that bout!")
   } else if (result === "t") {
-    ++tie
+    ++tie // point towards a tie
     console.log("Tied! Go again!")
   } else {
-    console.log("error :(");
+    console.log(); // fallthrough
   }
 
-  console.log(playerScore)
-  console.log(computerScore)
-  console.log(tie)
+  //console.log(playerScore)   // debugger
+  //console.log(computerScore) // debugger
+  //console.log(tie)           // debugger
 
+  // declares a win, loss, or tie
   if(playerScore === 3 || (i === 4 && playerScore > computerScore)) {
     console.log("Hurrah! You won the game!")
     break;
@@ -85,6 +125,6 @@ for(var i = 0; i < 5; ++i) {
   } else if (tie === 3) {
     console.log("It's a tie! Try again next time!")
   } else {
-    console.log()
+    console.log() // fallthrough
   }
 }
